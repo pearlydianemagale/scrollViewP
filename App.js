a9fca95cd09a786dd6c2860a5f787523b47af4f0
pearlydianemagale <magalepearlydiane@gmail.com>
2023-11-22 18:36:08 +0800

    1st up

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { ScrollView } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { users } from "./user";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.containers}>
+      <Text style={styles.name}s>LIST OF GUESTS</Text>
+      <ScrollView >
+        {users.map((users) => {
+          return (
+            <View>
+              <Text style={styles.name}>
+                {users.name.lastname},{users.name.firstname}{users.name.middlename ? `, ${users.name.middlename}.` : ''}
+              </Text>
+              <Text style={styles.address}>{users.address}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flexDirection: 'column',
+   justifyContent: 'flex-end'
+
+    },
+  name:{
+    color: "black",
+    fontWeight: 'bold',
+    fontSize: 20,
+   
   },
+  names:{
+    color: "black",
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginBottom: 40
+  },
+  address:{
+    color: "black",
+    fontSize: 15,
+    marginBottom: 15
+  },
+  containers:{
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    marginLeft: 20,
+    gap: 50,
+    
+  }
 });
---
+++ user.jsx
@@ -0,0 +1,83 @@
+export const users = [
+    {
+      id: 1,
+      name: {
+        firstname: "SHEILO",
+        middlename: "",
+        lastname: "SARAGA",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 2,
+      name: {
+        firstname: "JAMES WARREN",
+        middlename: "",
+        lastname: "BUCIA",
+      },
+      address: "Las Vegas, Nevada",
+    },
+    {
+      id: 3,
+      name: {
+        firstname: "RIA MAE",
+        middlename: "",
+        lastname: "ANGCAHAN",
+      },
+      address: "Paris, France",
+    },
+    {
+      id: 4,
+      name: {
+        firstname: "SHERLINEROSE",
+        middlename: "TANEO",
+        lastname: "DONDOYANO",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 5,
+      name: {
+        firstname: "MARY ANN",
+        middlename: "",
+        lastname: "DELA CRUZ",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 6,
+      name: {
+        firstname: "JAYNIE ",
+        middlename: "",
+        lastname: "ISRAEL",
+      },
+      address: "Las Vegas, Nevada",
+    },
+    {
+      id: 7,
+      name: {
+        firstname: "DEBORAH JANE",
+        middlename: "",
+        lastname: "ANTOPINA",
+      },
+      address: "Paris, France",
+    },
+    {
+      id: 8,
+      name: {
+        firstname: "ARNELL ",
+        middlename: "",
+        lastname: "PAJOTA",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 9,
+      name: {
+        firstname: "PEARLY DIANE",
+        middlename: "BEBERO",
+        lastname: "MAGALE",
+      },
+      address: "Los Angeles, California",
+    },
+  ];
